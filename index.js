4c7f276fefdc9800b545abb6e29bfd02d30a7768
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-03-17 15:10:24 +0800

    Index

---
+++ index.js
@@ -0,0 +1,12 @@
+function showNav()
+{
+	var x = document.getElementById('Topnav');
+	if (x.className === "topnav")
+	{
+		x.className += " responsive";
+	}
+	else
+	{
+		x.className = "topnav";
+	}
+}
